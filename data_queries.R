f90ee570dcf360bc45ab935517aa3f202cbcbc72
bellerho <beatrice.ellerhoff@iup.uni-heidelberg.de>
2020-04-11 21:56:14 +0200

    data queries return list joined.fail and response

--- data_queries.R
+++ data_queries.R
@@ -94,17 +94,37 @@ query_gform_measures <- function() {
             overwrite = TRUE
         )
         response <- read_csv(file = gfiles$measuresr) %>% 
-            select(matches("Wann wurden die Maßnahmen|Postleitzahl|Um welche Maßnahme|Erzähle uns mehr|wieder aufgehoben|Wann wurde die Maßnahme aufgehoben")) %>% rename(wann=1, plz=2, was=3, info=4, aufgehoben=5, wann_aufgehoben=6)
+          select(matches("Wann wurden die Maßnahmen|Postleitzahl|Stadt|Um welche Maßnahme|Erzähle uns mehr|wieder aufgehoben|Wann wurde die Maßnahme aufgehoben")) %>% rename(wann=1, plz=2, stadt=3, was=4, info=5, aufgehoben=6, wann_aufgehoben=7)
         
-        gverzeichnis.lookup <- read_delim("data/data_landkreise/gemeindeverzeichnis.csv", ";", escape_double = FALSE, trim_ws = TRUE)  %>% 
-            select(matches("Amtl.Gemeindeschlüssel|PLZ Ort")) %>% rename(schlüssel=1, plz_ort=2)
+        gverzeichnis.lookup <- read_delim(paste0(getwd(), "/data/data_landkreise/gemeindeverzeichnis.csv"), ";", escape_double = FALSE, trim_ws = TRUE)  %>% 
+          select(matches("Amtl.Gemeindeschlüssel|PLZ Ort")) %>% rename(schlüssel=1, plz_ort=2)
         
         gverzeichnis <- tibble(
             plz = as.numeric(unlist(regmatches(gverzeichnis.lookup$plz_ort, gregexpr("[[:digit:]]+", gverzeichnis.lookup$plz_ort)))),
+            stadt = sub("^\\s+", "", gsub('[[:digit:]]+', '', gverzeichnis.lookup$plz_ort)),
             IdLandkreis = gverzeichnis.lookup$schlüssel %>% substr(1, 5)
         )
         
-        response <- response %>% left_join(gverzeichnis) %>% filter(was != "sonstiges")
-        return(response)
+        response <- response %>% as_tibble() 
+        
+        #join by plz  
+        joined.plz <- response %>% select(-stadt) %>% inner_join(gverzeichnis, )
+        
+        #join by stadt
+        joined.stadt <- response %>% select(-plz) %>% inner_join(gverzeichnis, )
+        
+        #merge: filtered result
+        result <- full_join(joined.plz, joined.stadt) %>% filter(was != "sonstiges")  %>% filter(!is.na(IdLandkreis))
+        
+        #failed matches, here post-processing is required
+        joined.fail <- anti_join(
+          response,
+          result %>% select(-plz, -stadt))
+        
+        l <- list()
+        l$result <- result
+        l$joined.fail <- joined.fail
+        
+        return(l)
     }
 }
